042b13ed97f3d1f79c60d9bdf52abe08c57bed61
Jani Ärväs <31058046+oldfarts@users.noreply.github.com>
2020-04-28 07:31:53 +0300

    Updated ConsoleMACAddress.cpp

--- ConsoleMACAddress.cpp
+++ ConsoleMACAddress.cpp
@@ -16,12 +16,6 @@
 using namespace std;
 
 
-#include <stdio.h>
-#include <Windows.h>
-#include <Iphlpapi.h>
-#include <Assert.h>
-#pragma comment(lib, "iphlpapi.lib")
-
 #include <iostream>
 #include <string>
 #include <cstring>
@@ -36,32 +30,37 @@ using namespace std;
 
 
 int main() {
+    string com;
+    cout << "Give COM port for connection: " << endl;
+    cin >> com;
     CSerial serial;
-    serial.Open(_T("COM5"),GENERIC_READ);
+    if (com == "COM5") {
+        serial.Open(_T("COM5"), GENERIC_READ);
+    }
+    else {
+        cout << "Not correct COM port" << endl;
+        exit(0);
+    }
     if (!serial.IsOpen()) {
         printf("Serial is not open.");
         exit(0);
     }
     // Setup the serial port (9600,N81) using hardware handshaking
-    serial.Setup(CSerial::EBaud9600, CSerial::EData8, CSerial::EParNone, CSerial::EStop1);
+    serial.Setup(CSerial::EBaud115200, CSerial::EData8, CSerial::EParNone, CSerial::EStop1);
     serial.SetupHandshaking(CSerial::EHandshakeHardware);
     serial.SetupReadTimeouts(CSerial::EReadTimeoutNonblocking);
     while (1)
     {
-        //int byData = ReadByte(port);
-        //cout << ":" << byData;
-        //Sleep(1000);
-        DWORD dwBytesRead = 18;
-        BYTE  abBuffer[18];
+        DWORD dwBytesRead = 100;
+        BYTE  abBuffer[100];
             do
         {
-            // Read data from the COM-port
+                // Read data from the COM-port (COM5 port manually set)
                 serial.Read(abBuffer, sizeof(abBuffer), &dwBytesRead);
-                    unsigned con[6];
+                    //unsigned con[6];
                     string mac((char*)abBuffer);
                     if (!mac.empty()) {
                         cout << "Mac: " << mac << endl;
-                      //  Sleep(3000);
                     }
                     /*int c = sscanf(mac.c_str(), "%x %x %x %x %x %x", 
                         con, con + 1, con + 2, con + 3, con + 4, con + 5);
